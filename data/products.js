0e46f94ad0642516ac2756cdb78f6e526653cfb2
Darshana Das K <97202413+Darshana-Das-K@users.noreply.github.com>
2023-09-13 20:22:22 +0530

    Add files via upload

---
+++ data/products.js
@@ -0,0 +1,395 @@
+export const products = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/pngwing.com-4.png",
+    name: "High metallic plant pots - 2",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+    keywords: [
+      "pots",
+      "gardening",
+      "plant"
+    ]
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images/products/pngwing.com-3.png",
+    name: "Antique Globe",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "globe",
+      "world"
+    ]
+  },
+  {
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/products/NicePng_harry-potter-books-png_7148735.png",
+    name: "Harry Potter and the Sorcerer's Stone",
+    rating: {
+      stars: 4.5,
+      count: 56
+    },
+    priceCents: 799,
+    keywords: [
+      "book",
+      "used",
+      "harry potter"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/âPngtreeâantique chandelier chandelier_5651299.png"
+  },
+  {
+    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    image: "images/products/chandelier.png",
+    name: "2 Chandelier",
+    rating: {
+      stars: 5,
+      count: 2197
+    },
+    priceCents: 1899,
+    keywords: [
+      "chandelier",
+      "home",
+      "decorative"
+    ]
+  },
+  {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image: "images/products/6-piece-white-dinner-plate-set.jpg",
+    name: "6 Piece White Dinner Plate Set: Unused",
+    rating: {
+      stars: 4,
+      count: 37
+    },
+    priceCents: 2067,
+    keywords: [
+      "plates",
+      "kitchen",
+      "dining"
+    ]
+  },
+  {
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    image: "images/products/6-piece-non-stick-baking-set.webp",
+    name: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
+    rating: {
+      stars: 4.5,
+      count: 175
+    },
+    priceCents: 3499,
+    keywords: [
+      "kitchen",
+      "cookware"
+    ]
+  },
+  {
+    id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    image: "images/products/pngwing.com-2.png",
+    name: "Antique Telephone",
+    rating: {
+      stars: 4.5,
+      count: 317
+    },
+    priceCents: 2400,
+    keywords: [
+      "telephone",
+      "antique",
+      "beautiful"
+    ]
+  },
+  {
+    id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+    image: "images/products/pngwing.com.png",
+    name: "Table",
+    rating: {
+      stars: 4.5,
+      count: 144
+    },
+    priceCents: 3599,
+    keywords: [
+      "table",
+      "antique",
+      "wood",
+      "Table"
+    ]
+  },
+  
+ 
+  {
+    id: "aad29d11-ea98-41ee-9285-b916638cac4a",
+    image: "images/products/round-sunglasses-black.jpg",
+    name: "Round Sunglasses",
+    rating: {
+      stars: 4.5,
+      count: 30
+    },
+    priceCents: 1560,
+    keywords: [
+      "accessories",
+      "shades"
+    ]
+  },
+  
+  {
+    id: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
+    image: "images/products/blackout-curtain-set-beige.webp",
+    name: "Blackout Curtains Set 4-Pack - Beige",
+    rating: {
+      stars: 4.5,
+      count: 232
+    },
+    priceCents: 4599,
+    keywords: [
+      "bedroom",
+      "curtains",
+      "home"
+    ]
+  },
+ 
+  {
+    id: "c2a82c5e-aff4-435f-9975-517cfaba2ece",
+    image: "images/products/electric-glass-and-steel-hot-water-kettle.webp",
+    name: "Electric Glass and Steel Hot Tea Water Kettle - 1.7-Liter",
+    rating: {
+      stars: 5,
+      count: 846
+    },
+    priceCents: 3074,
+    keywords: [
+      "water boiler",
+      "appliances",
+      "kitchen"
+    ]
+  },
+  
+  {
+    id: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
+    image: "images/products/straw-sunhat.webp",
+    name: "Straw Lifeguard Sun Hat",
+    rating: {
+      stars: 4,
+      count: 215
+    },
+    priceCents: 2200,
+    keywords: [
+      "hats",
+      "straw hats",
+      "summer",
+      "apparel"
+    ]
+  },
+  {
+    id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
+    image: "images/products/sky-flower-stud-earrings.webp",
+    name: "Sterling Silver Sky Flower Stud Earrings",
+    rating: {
+      stars: 4.5,
+      count: 52
+    },
+    priceCents: 1799,
+    keywords: [
+      "jewelry",
+      "accessories",
+      "womens"
+    ]
+  },
+  
+  {
+    id: "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
+    image: "images/products/trash-can-with-foot-pedal-50-liter.jpg",
+    name: "Trash Can with Foot Pedal - Brushed Stainless Steel",
+    rating: {
+      stars: 4.5,
+      count: 2286
+    },
+    priceCents: 8300,
+    keywords: [
+      "garbage",
+      "bins",
+      "cans",
+      "kitchen"
+    ]
+  },
+  {
+    id: "19c6a64a-5463-4d45-9af8-e41140a4100c",
+    image: "images/products/duvet-cover-set-blue-twin.jpg",
+    name: "Duvet Cover Set with Zipper Closure",
+    rating: {
+      stars: 4,
+      count: 456
+    },
+    priceCents: 2399,
+    keywords: [
+      "bedroom",
+      "bed sheets",
+      "sheets",
+      "covers",
+      "home"
+    ]
+  },
+  {
+    id: "d2785924-743d-49b3-8f03-ec258e640503",
+    image: "images/products/women-chunky-beanie-gray.webp",
+    name: "Women's Chunky Cable Beanie - Gray",
+    rating: {
+      stars: 5,
+      count: 83
+    },
+    priceCents: 1250,
+    keywords: [
+      "hats",
+      "winter hats",
+      "beanies",
+      "tuques",
+      "apparel",
+      "womens"
+    ]
+  },
+ 
+  {
+    id: "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
+    image: "images/products/men-navigator-sunglasses-brown.jpg",
+    name: "Men's Navigator Sunglasses Pilot",
+    rating: {
+      stars: 3.5,
+      count: 42
+    },
+    priceCents: 1690,
+    keywords: [
+      "sunglasses",
+      "glasses",
+      "accessories",
+      "shades"
+    ]
+  },
+  {
+    id: "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
+    image: "images/products/non-stick-cooking-set-15-pieces.webp",
+    name: "Non-Stick Cookware Set, Pots, Pans and Utensils - 15 Pieces",
+    rating: {
+      stars: 4.5,
+      count: 511
+    },
+    priceCents: 6797,
+    keywords: [
+      "cooking set",
+      "kitchen"
+    ]
+  },
+  {
+    id: "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
+    image: "images/products/vanity-mirror-silver.jpg",
+    name: "Vanity Mirror with Heavy Base - Chrome",
+    rating: {
+      stars: 4.5,
+      count: 130
+    },
+    priceCents: 1649,
+    keywords: [
+      "bathroom",
+      "washroom",
+      "mirrors",
+      "home"
+    ]
+  },
+  
+  {
+    id: "d339adf3-e004-4c20-a120-40e8874c66cb",
+    image: "images/products/double-elongated-twist-french-wire-earrings.webp",
+    name: "Double Oval Twist French Wire Earrings - Gold",
+    rating: {
+      stars: 4.5,
+      count: 117
+    },
+    priceCents: 2400,
+    keywords: [
+      "accessories",
+      "womens"
+    ]
+  },
+  {
+    id: "d37a651a-d501-483b-aae6-a9659b0757a0",
+    image: "images/products/round-airtight-food-storage-containers.jpg",
+    name: "Round Airtight Food Storage Containers - 5 Piece",
+    rating: {
+      stars: 4,
+      count: 126
+    },
+    priceCents: 2899,
+    keywords: [
+      "boxes",
+      "food containers",
+      "kitchen"
+    ]
+  },
+  {
+    id: "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
+    image: "images/products/coffeemaker-with-glass-carafe-black.jpg",
+    name: "Coffeemaker with Glass Carafe and Reusable Filter - 25 Oz, Black",
+    rating: {
+      stars: 4.5,
+      count: 1211
+    },
+    priceCents: 2250,
+    keywords: [
+      "coffeemakers",
+      "kitchen",
+      "appliances"
+    ]
+  },
+  {
+    id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
+    image: "images/products/blackout-curtains-black.jpg",
+    name: "Blackout Curtains Set 42 x 84-Inch - Black, 2 Panels",
+    rating: {
+      stars: 4.5,
+      count: 363
+    },
+    priceCents: 3099,
+    keywords: [
+      "bedroom",
+      "home"
+    ]
+  },
+  
+  {
+    id: "77a845b1-16ed-4eac-bdf9-5b591882113d",
+    image: "images/products/countertop-blender-64-oz.jpg",
+    name: "Countertop Blender - 64oz, 1400 Watts",
+    rating: {
+      stars: 4,
+      count: 3
+    },
+    priceCents: 10747,
+    keywords: [
+      "food blenders",
+      "kitchen",
+      "appliances"
+    ]
+  },
+  {
+    id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
+    image: "images/products/floral-mixing-bowl-set.jpg",
+    name: "10-Piece Mixing Bowl Set with Lids - Floral",
+    rating: {
+      stars: 5,
+      count: 679
+    },
+    priceCents: 3899,
+    keywords: [
+      "mixing bowls",
+      "baking",
+      "cookware",
+      "kitchen"
+    ]
+  },
+  
+  
+];
